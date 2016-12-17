f23b80b3c20d6e6e556ef96901c2ca64175b1c13
Zsolt Meszaros <zsolt@sowad.com>
2016-12-17 14:22:05 +0000

    Add record/play/stop

--- src/js/game.js
+++ src/js/game.js
@@ -1,58 +1,145 @@
 'use strict';
 
-const notes = document.getElementsByTagName('audio');
-const keys = document.getElementById('game').getElementsByTagName('div');
-let recording = false;
+function Game() {
+  const dom = {};
+  let timer;
+  let sequence;
+  let state;
 
-for (let i = 0; i < keys.length; i++) {
-  keys[i].addEventListener('click', _ => notes[i].play());
+  function init() {
+    cacheDom();
+    bindEvents();
+    timer = Timer();
+    sequence = Sequence(dom.sounds);
+  }
+  function cacheDom() {
+    dom.game = document.getElementById('game');
+    dom.keys = game.getElementsByTagName('div');
+    dom.sounds = {
+      c: document.getElementById('c'),
+      d: document.getElementById('d'),
+      e: document.getElementById('e'),
+      f: document.getElementById('f'),
+      g: document.getElementById('g'),
+      a: document.getElementById('a'),
+      b: document.getElementById('b'),
+      cc: document.getElementById('cc')
+    }
+    dom.record = document.getElementById('record');
+    dom.play = document.getElementById('play');
+    dom.stop = document.getElementById('stop');
+  }
+  function bindEvents() {
+    dom.game.addEventListener('click', playNote);
+    dom.record.addEventListener('click', record);
+    dom.play.addEventListener('click', play);
+    dom.stop.addEventListener('click', stop);
+  }
+  function playNote() {
+    if (event.target === dom.game || state === 'playing') {
+      return;
+    }
+    let note = event.target.dataset.key;
+    if (state === 'recording') {
+      sequence.record(note, timer.current());
+    }
+    dom.sounds[note].play();
+  }
+  function record() {
+    state = 'recording';
+    timer.start();
+  }
+  function play() {
+    state = 'playing';
+    sequence.play();
+  }
+  function stop() {
+    state = undefined;
+    sequence.stop();
+  }
+  return {
+    init
+  }
 }
 
-// record key hit
-// record timing
-if (recording) {
+function Timer() {
+  let startedAt;
+  let timeouts = [];
 
+  function start() {
+    startedAt = new Date();
+  }
+  function current() {
+    return new Date() - startedAt;
+  }
+  function wait(time) {
+    return new Promise((resolve, reject) => {
+      timeouts.push(setTimeout(resolve, time));
+    });
+  }
+  function clear() {
+    for (let i = 0; i < timeouts.length; i++) {
+      clearTimeout(timeouts[i]);
+    }
+  }
+  return {
+    start,
+    current,
+    wait,
+    clearAll: clear
+  }
 }
 
-function Sequence() {
+function Sequence(notes) {
+  let timer = Timer();
+  let timeouts = [];
   let sequence = [
-    {note: 1, time: 500},
-    {note: 6, time: 1000},
-    {note: 5, time: 1500},
-    {note: 4, time: 2000},
-    {note: 1, time: 2500},
-    {note: 1, time: 4500},
-    {note: 1, time: 4700},
-    {note: 1, time: 5000},
-    {note: 6, time: 5500},
-    {note: 5, time: 6000},
-    {note: 4, time: 6500},
-    {note: 2, time: 7000}
+    // {note: 'd', time: 500},
+    // {note: 'b', time: 1000},
+    // {note: 'a', time: 1500},
+    // {note: 'g', time: 2000},
+    // {note: 'd', time: 2500},
+    // {note: 'd', time: 4500},
+    // {note: 'd', time: 4700},
+    // {note: 'd', time: 5000},
+    // {note: 'b', time: 5500},
+    // {note: 'a', time: 6000},
+    // {note: 'g', time: 6500},
+    // {note: 'e', time: 7000},
+    // {note: 'e', time: 9000},
+    // {note: 'cc', time: 9500},
+    // {note: 'b', time: 10000},
+    // {note: 'a', time: 10500},
+    // {note: 'g', time: 11000}
   ];
   function play() {
     for (let i = 0; i < sequence.length; i++) {
-      wait(sequence[i].time)
-      // .then(wait(1000))
+      console.log(sequence[i])
+      // console.log(notes[sequence[i].note])
+      timer.wait(sequence[i].time)
       .then(_ => notes[sequence[i].note].pause())
       .then(_ => notes[sequence[i].note].currentTime = 0)
       .then(_ => notes[sequence[i].note].play())
     }
   }
-  function record() {}
-  function stop() {}
-  function wait(time) {
-    return new Promise((resolve, reject) => {
-      setTimeout(resolve, time);
-    });
+  function record(note, time) {
+    sequence.push({note: note, time: time});
+  }
+  function stop() {
+    timer.clearAll();
+  }
+  function clear() {
+    sequence = [];
   }
   return {
-    play
+    play,
+    record,
+    stop,
+    clear
   }
 }
 
-function playSequence() {}
-function recordSequence() {
-  // check time between actions
-  // when hit a key, record key and time elapsed
-
-}
+document.addEventListener('DOMContentLoaded', function(e) {
+  const game = Game();
+  game.init();
+});
